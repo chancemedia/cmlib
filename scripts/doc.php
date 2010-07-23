f78ebb20016fff2d3b8edb80170e2f9660bf3194
Elliot Chance <elliot@chancemedia.com>
2010-07-23 14:38:45 +1000

    doc.php is now safe when switching branches with uncommitted changes.

--- scripts/doc.php
+++ scripts/doc.php
@@ -7,7 +7,13 @@ system("git checkout master");
 system("/Applications/Doxygen.app/Contents/Resources/doxygen Doxyfile");
 
 // because doc/ is not being tracked by master we can switch branches and keep doc/ intact
-system("git checkout --force gh-pages");
+system("git checkout gh-pages", $error);
+if($error != 0)
+	die("FAILED: git checkout gh-pages\n");
+
+// copy doc_temp/ over doc/
+system("rm -rf doc");
+system("mv doc_temp doc");
 
 // commit changes
 system("git add . && git commit -a -m \"Docs - `date`\"");
