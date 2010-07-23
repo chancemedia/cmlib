a2c124fb2c2a49af3eabe0fd57c468c4da545560
Elliot Chance <elliot@chancemedia.com>
2010-07-23 13:06:53 +1000

    Docs - Fri 23 Jul 2010 13:06:53 EST

--- scripts/doc.php
+++ scripts/doc.php
@@ -1,9 +1,21 @@
 <?php
 
 // make sure we are on the master branch
-//system("git checkout master");
+system("git checkout master");
 
 // generate the documentation
 system("/Applications/Doxygen.app/Contents/Resources/doxygen Doxyfile");
+
+// because doc/ is not being tracked by master we can switch branches and keep doc/ intact
+system("git checkout gh-pages");
+
+// commit changes
+system("git add . && git commit -a -m \"Docs - `date`\"");
+
+// push changes
+system("git push");
+
+// switch back to master branch
+system("git checkout master");
 
 ?>
