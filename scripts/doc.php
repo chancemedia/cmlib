a53237dd2acc9d0529ef53b79f75687ce146a006
Elliot Chance <elliot@chancemedia.com>
2010-07-23 14:50:00 +1000

    doc.php will keep a copy of doc_temp for browsing on the master branch.

--- scripts/doc.php
+++ scripts/doc.php
@@ -4,22 +4,29 @@
 system("git checkout master");
 
 // generate the documentation
-system("/Applications/Doxygen.app/Contents/Resources/doxygen Doxyfile");
+system("rm -rf doc_temp");
+system("/Applications/Doxygen.app/Contents/Resources/doxygen Doxyfile", $error);
+if($error != 0)
+	die("FAILED: Doxygen\n");
 
 // because doc/ is not being tracked by master we can switch branches and keep doc/ intact
 system("git checkout gh-pages", $error);
 if($error != 0)
 	die("FAILED: git checkout gh-pages\n");
 
-// copy doc_temp/ over doc/
+// remove old doc/
 system("rm -rf doc");
-system("mv doc_temp doc");
+
+// we copy rather than move because we want to keep doc_temp/ for browsing on the master branch
+system("cp -R doc_temp doc");
 
 // commit changes
 system("git add . && git commit -a -m \"Docs - `date`\"");
 
 // push changes
-system("git push");
+system("git push", $error);
+if($error != 0)
+	die("FAILED: git push\n");
 
 // switch back to master branch
 system("git checkout master");
