ef4422d3375d8344a754eb066e45e64057e24b56
Elliot Chance <elliot@chancemedia.com>
2010-07-23 13:01:05 +1000

    Preparation for gh-pages branch

---
+++ scripts/doc.php
@@ -0,0 +1,9 @@
+<?php
+
+// make sure we are on the master branch
+//system("git checkout master");
+
+// generate the documentation
+system("/Applications/Doxygen.app/Contents/Resources/doxygen Doxyfile");
+
+?>
