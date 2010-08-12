7c6c485351d1e50dd18e739233b663d0e21873f4
Elliot Chance <elliot@chancemedia.com>
2010-08-12 10:39:17 +1000

    Added CMColor class.

---
+++ lib/CMColor.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @brief An abstract class for colors using RGB.
+ * @author Elliot Chance
+ */
+class CMColor {
+	
+	/**
+	 * @brief Black.
+	 */
+	public static $BLACK = array(0, 0, 0);
+	
+	/**
+	 * @brief White.
+	 */
+	public static $WHITE = array(255, 255, 255);
+	
+}
+
+?>
