2be7119e3d7ccef0b48f35fba5f7a96ba99862fa
Elliot Chance <elliot@chancemedia.com>
2010-08-12 12:03:59 +1000

    More drawing functions.

--- lib/CMGraphicDraw.php
+++ lib/CMGraphicDraw.php
@@ -1,9 +1,16 @@
 <?php
 
 include_once('CMGraphic.php');
+include_once('CMColor.php');
 
 /**
  * @brief CMGraphicDraw interface defines the methods for drawing image files.
+ * 
+ * Where arguments for red, green, blue or alpha are permitted you may use the absolute value
+ * (0 - 255 for RGB and 0 - 127 for A) or use a percentage betwen 0.0 and LESS THAN 1.0 which
+ * will automatically be translated into the corresponding absolute value. 1.0 will be
+ * interpreted as an absolute value of 1 which will not be translated into 255.
+ * 
  * @author Elliot Chance
  */
 class CMGraphicDraw extends CMGraphic {
@@ -18,7 +25,12 @@ class CMGraphicDraw extends CMGraphic {
 	 */
 	private $brushColor = false;
 	
-	private $brushColorRGB = array(0, 0, 0);
+	/**
+	 * @brief The brush color in RGBA.
+	 * 
+	 * The default brush color is black.
+	 */
+	private $brushColorRGBA = array(0, 0, 0, 0);
 	
 	/**
 	 * @brief Anti alias enabled.
@@ -40,25 +52,77 @@ class CMGraphicDraw extends CMGraphic {
 	 */
 	public function CMGraphicDraw($file = false) {
 		parent::CMGraphic($file);
+		
+		// if we were given a file location then we need to create the resource from that
+		if($this->getImageType() == "GIF")
+			$this->resource = imagecreatefromgif($file);
+		else if($this->getImageType() == "JPEG")
+			$this->resource = imagecreatefromjpeg($file);
+		else if($this->getImageType() == "PNG")
+			$this->resource = imagecreatefrompng($file);
+			
+		// set the brush color to black
+		$this->brushColor = imagecolorallocatealpha($this->resource, 0, 0, 0, 0);
+	}
+	
+	/**
+	 * @brief Convert absolute or percentage color components into absolute color components.
+	 * 
+	 * @param $red Red value (0 - 255 OR 0.0 - 1.0)
+	 * @param $green Green value (0 - 255 OR 0.0 - 1.0)
+	 * @param $blue Blue value (0 - 255 OR 0.0 - 1.0)
+	 * @param $alpha Alpha value (0 - 127 OR 0.0 - 1.0)
+	 * @return A new array with 4 elements representing red, green, blue and alpha respectivly.
+	 */
+	public function convertColorToAbsolute($red, $green, $blue, $alpha = 0) {
+		if($red < 1)
+			$red *= 255;
+		if($green < 1)
+			$green *= 255;
+		if($blue < 1)
+			$blue *= 255;
+		if($alpha < 1)
+			$alpha *= 127;
+		
+		return array($red, $green, $blue, $alpha);
 	}
 	
 	/**
 	 * @brief Set the current brush color.
-	 * @param $red Red value (0 - 255)
-	 * @param $green Green value (0 - 255)
-	 * @param $blue Blue value (0 - 255)
+	 * 
+	 * @param $red Red value (0 - 255 OR 0.0 - 1.0)
+	 * @param $green Green value (0 - 255 OR 0.0 - 1.0)
+	 * @param $blue Blue value (0 - 255 OR 0.0 - 1.0)
+	 * @param $alpha Alpha value (0 - 127 OR 0.0 - 1.0)
 	 * @return The current brush colour as created by imagecolorallocate().
+	 * @see getBrushColor()
 	 */
-	public function setBrushColor($red, $green, $blue) {
+	public function setBrushColor($red, $green, $blue, $alpha = 0) {
 		if($this->resource === false)
 			return false;
 			
-		$this->brushColor = imagecolorallocate($this->resource, $red, $green, $blue);
+		// translate colours
+		list($red, $green, $blue, $alpha) = $this->brushColorRGBA =
+			$this->convertColorToAbsolute($red, $green, $blue, $alpha);
+		
+		// set bruch color
+		$this->brushColor = imagecolorallocatealpha($this->resource, $red, $green, $blue, $alpha);
 		return $this->brushColor;
 	}
 	
 	/**
+	 * @brief Get the current brush color.
+	 * 
+	 * @return An array of 3 elements in the order of red, green and blue.
+	 * @see setBrushColor()
+	 */
+	public function getBrushColor() {
+		return $this->brushColorRGB;
+	}
+	
+	/**
 	 * @brief Draw an arc.
+	 * 
 	 * @param $cx x-coordinate of the center.
 	 * @param $cy y-coordinate of the center.
 	 * @param $width The arc width.
@@ -73,12 +137,24 @@ class CMGraphicDraw extends CMGraphic {
 	public function drawArc($cx, $cy, $width, $height, $start, $end, $a = array()) {
 		if($this->resource === false)
 			return false;
-			
-		// set the brush color to black if one has not been set
-		if($this->brushColor === false)
-			$this->brushColor = imagecolorallocate($this->resource, 0, 0, 0);
 		
 		return imagearc($this->resource, $cx, $cy, $width, $height, $start, $end, $this->brushColor);
+	}
+	
+	/**
+	 * @brief Draw a rectangle.
+	 * 
+	 * @param $x1 Upper left x coordinate.
+	 * @param $y1 Upper left y coordinate 0, 0 is the top left corner of the image.
+	 * @param $x2 Bottom right x coordinate.
+	 * @param $y2 Bottom right y coordinate.
+	 * @return \true on success, otherwise \false.
+	 */
+	public function drawRectangle($x1, $y1, $x2, $y2) {
+		if($this->resource === false)
+			return false;
+		
+		return imagerectangle($this->resource, $x1, $y1, $x2, $y2, $this->brushColor);
 	}
 	
 	/**
@@ -106,6 +182,7 @@ class CMGraphicDraw extends CMGraphic {
 	
 	/**
 	 * @brief Find if alpha blending is enabled.
+	 * 
 	 * @return \true or \false.
 	 */
 	public function getAlphaBlending() {
@@ -138,10 +215,51 @@ class CMGraphicDraw extends CMGraphic {
 	
 	/**
 	 * @brief Find if anti aliasing is enabled.
+	 * 
 	 * @return \true or \false.
 	 */
 	public function getAntiAlias() {
 		return $this->antialias;
+	}
+	
+	/**
+	 * @brief Get the RGBA of a single pixel.
+	 * 
+	 * Returns the index of the color of the pixel at the specified location.
+	 * 
+	 * If PHP is compiled against GD library 2.0 or higher and the image is a truecolor image,
+	 * this function returns the RGBA value of that pixel as integer.
+	 * 
+	 * @param $x x-coordinate of the point.
+	 * @param $y y-coordinate of the point.
+	 * @return A four element array containing red, green, blue and alpha respectivly.
+	 */
+	public function colorAtPixel($x, $y) {
+		if($this->resource === false)
+			return false;
+		
+		$rgb = imagecolorat($this->resource, $x, $y);
+		$r = ($rgb >> 16) & 0xFF;
+		$g = ($rgb >> 8) & 0xFF;
+		$b = $rgb & 0xFF;
+		
+		return array($r, $g, $b, 0);
+	}
+	
+	/**
+	 * @brief Draw a line.
+	 * 
+	 * @param $x1 x-coordinate for first point.
+	 * @param $y1 y-coordinate for first point.
+	 * @param $x2 x-coordinate for second point.
+	 * @param $y2 y-coordinate for second point.
+	 * @return \true on success, otherwise \false.
+	 */
+	public function drawLine($x1, $y1, $x2, $y2) {
+		if($this->resource === false)
+			return false;
+		
+		return imageline($this->resource, $x1, $y1, $x2, $y2, $this->brushColor);
 	}
 	
 }
