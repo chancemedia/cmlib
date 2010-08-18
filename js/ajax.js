681a15aadb2db1f3a266bb8efbb637038a3c40f7
Elliot Chance <elliot@chancemedia.com>
2010-08-18 18:44:42 +1000

    Minor

--- js/ajax.js
+++ js/ajax.js
@@ -10,14 +10,17 @@ function createAjaxHandle() {
 	try {
 		// Firefox, Opera 8.0+, Safari
 		xmlHttp = new XMLHttpRequest();
-	} catch (e) {
+	}
+	catch (e) {
 		// Internet Explorer
 		try {
 			xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (e) {
+		}
+		catch (e) {
 			try {
 				xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-			} catch (e) {
+			}
+			catch (e) {
 				alert("Your browser does not support AJAX!");
 				return false;
 			}
