d49d85cf1bc085b70ce0a89a58e7d3219f07c124
Elliot Chance <elliot@chancemedia.com>
2010-08-20 16:59:55 +1000

    Much more dim and undimming functions.

--- js/ui.js
+++ js/ui.js
@@ -31,45 +31,70 @@ function _addWindowResizeObject(name, func) {
 	windowResizeStack.push(name, func);
 }
 
-function getWindowHeight() {
+function _windowHeight(docobj, winobj) {
 	// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-	if(typeof window.innerHeight != 'undefined')
-		return window.innerHeight;
+	if(typeof winobj.innerHeight != 'undefined')
+		return winobj.innerHeight;
 	
 	// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-	else if(typeof document.documentElement != 'undefined' && typeof document.documentElement.clientHeight != 'undefined' &&
-			document.documentElement.clientHeight != 0)
-		return document.documentElement.clientHeight;
+	else if(typeof docobj.documentElement != 'undefined' && typeof docobj.documentElement.clientHeight != 'undefined' &&
+			docobj.documentElement.clientHeight != 0)
+		return docobj.documentElement.clientHeight;
 		
 	// older versions of IE
 	else
-		return document.getElementsByTagName('body')[0].clientHeight;
+		return docobj.getElementsByTagName('body')[0].clientHeight;
+}
+
+function _windowWidth(docobj, winobj) {
+	// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+	if(typeof winobj.innerWidth != 'undefined')
+		return winobj.innerWidth;
+	
+	// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+	else if(typeof docobj.documentElement != 'undefined' && typeof docobj.documentElement.clientWidth != 'undefined' &&
+			docobj.documentElement.clientWidth != 0)
+		return docobj.documentElement.clientWidth;
+		
+	// older versions of IE
+	else
+		return docobj.getElementsByTagName('body')[0].clientWidth;
+}
+
+function getWindowHeight() {
+	// we must look up to all parent windows
+	if(window.parent.document != null)
+		return _windowHeight(window.parent.document, window.parent);
+	
+	return _windowHeight(document, window);
 }
 
 function getWindowWidth() {
-	// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-	if(typeof window.innerWidth != 'undefined')
-		return window.innerWidth;
+	// we must look up to all parent windows
+	if(window.parent.document != null)
+		return _windowWidth(window.parent.document, window.parent);
 	
-	// IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-	else if(typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth != 'undefined' &&
-			document.documentElement.clientWidth != 0)
-		return document.documentElement.clientWidth;
-		
-	// older versions of IE
-	else
-		return document.getElementsByTagName('body')[0].clientWidth;
+	return _windowWidth(document, window);
 }
 
-function undimWindow() {
+function _undim(obj) {
 	// if the dimmer doesn't exist then something went wrong, but we have to return
-	if(document.getElementById('window_dim') == null)
-		return;
+	if(obj.getElementById('window_dim') == null)
+		return false;
 		
-	var obj = document.getElementById('window_dim');
+	var obj = obj.getElementById('window_dim');
 	obj.style.backgroundColor = 'rgba(0, 0, 0, 0.0)';
 	obj.style.height = '0px';
 	obj.style.width = '0px';
+	return true;
+}
+
+function undimWindow() {
+	return _undim(document);
+}
+
+function undimParentWindow() {
+	return _undim(window.parent.document);
 }
 
 function _windowWrite(html) {
@@ -81,10 +106,10 @@ function _createWindowDim() {
 		_windowWrite('<div id="window_dim" style="">&nbsp;</div>');
 }
 
-function dimWindow(opacity, onclick) {
+function _dimWindow(docobj, opacity, onclick) {
 	_createWindowDim();
 	
-	var obj = document.getElementById('window_dim');
+	var obj = docobj.getElementById('window_dim');
 	obj.style.backgroundColor = "rgba(0, 0, 0, " + opacity + ")";
 	obj.style.top = '0px';
 	obj.style.left = '0px';
@@ -94,14 +119,22 @@ function dimWindow(opacity, onclick) {
 	
 	// resize with window
 	_addWindowResizeObject('window_dim', function() {
-		var obj = document.getElementById('window_dim');
+		var obj = docobj.getElementById('window_dim');
 		obj.style.height = getWindowHeight() + 'px';
 		obj.style.width = getWindowWidth() + 'px';
 	});
 	
 	// actions
 	if(onclick != null)
-		document.getElementById('window_dim').onclick = onclick;
+		docobj.getElementById('window_dim').onclick = onclick;
+}
+
+function dimWindow(opacity, onclick) {
+	return _dimWindow(document, opacity, onclick);
+}
+
+function dimParentWindow(opacity, onclick) {
+	return _dimWindow(window.parent.document, opacity, onclick);
 }
 
 function runTimeline(code, start, finish, totalFrames, totalTime) {
@@ -117,18 +150,35 @@ function runTimeline(code, start, finish, totalFrames, totalTime) {
 	}
 }
 
-function dimWindowSmooth(opacity, totalTime, onclick) {
+function _dimSmooth(obj, opacity, totalTime, onclick) {
 	_createWindowDim();
-	runTimeline('dimWindow(%)', 0.0, opacity, totalTime / 0.1, totalTime);
+	
+	if(obj == document)
+		runTimeline('dimWindow(%)', 0.0, opacity, totalTime / 0.1, totalTime);
+	else
+		runTimeline('dimParentWindow(%)', 0.0, opacity, totalTime / 0.1, totalTime);
 	
 	// actions
 	if(onclick != null)
-		document.getElementById('window_dim').onclick = onclick;
+		obj.getElementById('window_dim').onclick = onclick;
+}
+
+function dimWindowSmooth(opacity, totalTime, onclick) {
+	return _dimSmooth(document, opacity, totalTime, onclick);
+}
+
+function dimParentWindowSmooth(opacity, totalTime, onclick) {
+	return _dimSmooth(window.parent.document, opacity, totalTime, onclick);
 }
 
 function undimWindowSmooth(opacity, totalTime) {
 	runTimeline('dimWindow(%)', opacity, 0.0, totalTime / 0.1, totalTime);
 	setTimeout('undimWindow()', (totalTime * 1000) + 10);
+}
+
+function undimParentWindowSmooth(opacity, totalTime) {
+	runTimeline('dimParentWindow(%)', opacity, 0.0, totalTime / 0.1, totalTime);
+	setTimeout('undimParentWindow()', (totalTime * 1000) + 10);
 }
 
 function isNumeric(num) {
