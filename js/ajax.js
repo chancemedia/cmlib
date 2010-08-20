79aa63302e8550d79549e3306b18b12a9ba9ff3f
Elliot Chance <elliot@chancemedia.com>
2010-08-21 00:46:27 +1000

    Added submitAjaxForm() and submitAjax().

--- js/ajax.js
+++ js/ajax.js
@@ -7,6 +7,7 @@
  */
 function createAjaxHandle() {
 	var xmlHttp;
+	
 	try {
 		// Firefox, Opera 8.0+, Safari
 		xmlHttp = new XMLHttpRequest();
@@ -26,5 +27,33 @@ function createAjaxHandle() {
 			}
 		}
 	}
+	
 	return xmlHttp;
+}
+
+function submitAjaxForm(form, url) {
+	var objs = [];
+	for(var i = 0; i < form.elements.length; ++i)
+		objs[form.elements[i].name] = form.elements[i].value;
+	return submitAjax(url, objs);
+}
+
+function submitAjax(url, data) {
+	// make sure we always use a new handle, less efficient but much safer if multiple
+	// requests/results happen at once
+	var ajaxHandle = createAjaxHandle();
+	
+	// setup listener
+	ajaxHandle.onreadystatechange = function() {
+		if(ajaxHandle.readyState == 4)
+			alert(ajaxHandle.responseText);
+	}
+	
+	// submit request
+	ajaxHandle.open("POST", url, true);
+	ajaxHandle.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	var post = new Array();
+	for(key in data)
+		post.push(key + "=" + escape(data[key]));
+	ajaxHandle.send(post.join('&'));
 }
