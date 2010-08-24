40fc04549cf991939ee3e67ed84870259e218558
Elliot Chance <elliot@chancemedia.com>
2010-08-24 17:56:01 +1000

    Bug fixed where runTimeline() was not performing the last frame.

--- js/ui.js
+++ js/ui.js
@@ -143,7 +143,7 @@ function runTimeline(code, start, finish, totalFrames, totalTime) {
 	
 	// setup and execute timeline
 	var next = start;
-	for(var i = 0; i < totalFrames; ++i) {
+	for(var i = 0; i <= totalFrames; ++i) {
 		next += ((finish - start) / totalFrames);
 		var newcode = code.replace('%', next);
 		setTimeout(newcode, milli * i);
