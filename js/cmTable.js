106092f0cfe05e85ca74092bb216c35f0abae897
Elliot Chance <elliot@chancemedia.com>
2010-09-15 10:22:27 +1000

    cmTableAddRow() now uses correct JSON parsing to apply tr and td attributes.

--- js/cmTable.js
+++ js/cmTable.js
@@ -1,3 +1,6 @@
+// we need JSON2 for this
+cmLoadModule("json2");
+
 function cmTableTotalRows(tableName) {
 	if(document.getElementById(tableName) == null)
 		return 0;
@@ -8,12 +11,27 @@ function cmTableAddRow(tableName, cells, attr) {
 	var table = document.getElementById(tableName);
 	var row = table.insertRow(table.rows.length);
 	
-	if(attr.tr_id != null)
-		row.id = attr.tr_id;
+	if(attr != null) {
+		JSON.parse(JSON.stringify(attr), function(key, value) {
+			if(key == 'tr_style')
+				row.style.cssText = attr.tr_style;
+			else if(key.substr(0, 3) == 'tr_')
+				eval('row.' + key.substr(3) + ' = attr.' + key + ';');
+		});
+	}
 	
 	for(var i = 0; i < cells.length; ++i) {
 		var cell = row.insertCell(i);
 		cell.innerHTML = cells[i];
+		
+		if(attr != null) {
+			JSON.parse(JSON.stringify(attr), function(key, value) {
+				if(key == 'td_style')
+					cell.style.cssText = attr.td_style;
+				else if(key.substr(0, 3) == 'td_')
+					eval('cell.' + key.substr(3) + ' = attr.' + key + ';');
+			});
+		}
 	}
 }
 
