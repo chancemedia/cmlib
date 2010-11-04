3961e898e08997fa542b79ac31678484c386ea52
Elliot Chance <elliot@chancemedia.com>
2010-11-04 15:57:44 +1100

    Clean commit before version release.

--- srcdoc/manual_about_release.php
+++ srcdoc/manual_about_release.php
@@ -58,12 +58,13 @@ v1.3b</pre>
  * 
  * @section manual_about_release_release Release Procedure
  * 
- * 1. Clean commit on master branch.
- * 2. Run <tt>test/testall.php</tt>.
- * 3. Run <tt>scripts/doxygen.php</tt> to make sure there are no Doxygen warnings or errors.
- * 4. Run <tt>scripts/doc.php</tt> to generate and upload documentation.
- * 5. Create version tag on master branch and gh-pages branch.
- * 6. <tt>git push</tt> tags.
+ * 1. Run <tt>test/testall.php</tt>.
+ * 2. Update <tt>srcdoc/manual_about_test.php</tt> with full test output.
+ * 4. Run <tt>scripts/doxygen.php</tt> to make sure there are no Doxygen warnings or errors.
+ * 3. Clean commit on master branch.
+ * 5. Run <tt>scripts/doc.php</tt> to generate and upload documentation.
+ * 6. Create version tag on master branch and gh-pages branch.
+ * 7. <tt>git push</tt> tags.
  * 
  */
 
